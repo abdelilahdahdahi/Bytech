aae7566e6e24e88c83fe03eba965df29991706cf
abdelilah <dahdahiabdelilah2002@gmail.com>
2023-10-25 10:29:40 +0200

    created Model Articles

---
+++ backend/models/Articles.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use common\models\User;
+
+/**
+ * This is the model class for table "articles".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property int $created_by
+ * @property int $category_id
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @property Categories $category
+ * @property User $createdBy
+ */
+class Articles extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'articles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'created_by', 'category_id'], 'required'],
+            [['content'], 'string'],
+            [['created_by', 'category_id'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Categories::class, 'targetAttribute' => ['category_id' => 'id']],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'created_by' => 'Created By',
+            'category_id' => 'Category ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Categories::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+}
